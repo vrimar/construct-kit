de74a1d62bcf5d832ad0f8eb4287bc8d01623195
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2026-03-17 14:06:24 -0400

    fix(@b3/ui/theme): extend input-group padding selectors to button children

--- packages/ui/src/theme/recipes/input-group.ts
+++ packages/ui/src/theme/recipes/input-group.ts
@@ -30,49 +30,49 @@ export const inputGroup = defineSlotRecipe({
       "2xs": {
         element: { minW: "7", _icon: { boxSize: "3.5" } },
         root: {
-          "& > input:not(:first-child)": { ps: "6!" },
-          "& > input:not(:last-child)": { pe: "6!" },
+          "& > :is(input, button):not(:first-child)": { ps: "6!" },
+          "& > :is(input, button):not(:last-child)": { pe: "6!" },
         },
       },
       xs: {
         element: { minW: "8", _icon: { boxSize: "4" } },
         root: {
-          "& > input:not(:first-child)": { ps: "7!" },
-          "& > input:not(:last-child)": { pe: "7!" },
+          "& > :is(input, button):not(:first-child)": { ps: "7!" },
+          "& > :is(input, button):not(:last-child)": { pe: "7!" },
         },
       },
       sm: {
         root: {
-          "& > input:not(:first-child)": { ps: "8!" },
-          "& > input:not(:last-child)": { pe: "8!" },
+          "& > :is(input, button):not(:first-child)": { ps: "8!" },
+          "& > :is(input, button):not(:last-child)": { pe: "8!" },
         },
         element: { minW: "9", _icon: { boxSize: "4.5" } },
       },
       md: {
         root: {
-          "& > input:not(:first-child)": { ps: "9!" },
-          "& > input:not(:last-child)": { pe: "9!" },
+          "& > :is(input, button):not(:first-child)": { ps: "9!" },
+          "& > :is(input, button):not(:last-child)": { pe: "9!" },
         },
         element: { minW: "10", _icon: { boxSize: "5" } },
       },
       lg: {
         root: {
-          "& > input:not(:first-child)": { ps: "10!" },
-          "& > input:not(:last-child)": { pe: "10!" },
+          "& > :is(input, button):not(:first-child)": { ps: "10!" },
+          "& > :is(input, button):not(:last-child)": { pe: "10!" },
         },
         element: { minW: "11", _icon: { boxSize: "5" } },
       },
       xl: {
         root: {
-          "& > input:not(:first-child)": { ps: "11!" },
-          "& > input:not(:last-child)": { pe: "11!" },
+          "& > :is(input, button):not(:first-child)": { ps: "11!" },
+          "& > :is(input, button):not(:last-child)": { pe: "11!" },
         },
         element: { minW: "11", _icon: { boxSize: "5.5" } },
       },
       "2xl": {
         root: {
-          "& > input:not(:first-child)": { ps: "12!" },
-          "& > input:not(:last-child)": { pe: "12!" },
+          "& > :is(input, button):not(:first-child)": { ps: "12!" },
+          "& > :is(input, button):not(:last-child)": { pe: "12!" },
         },
         element: { minW: "12", _icon: { boxSize: "6" } },
       },
